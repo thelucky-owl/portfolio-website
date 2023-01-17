71d8a009c8092e9b1467922798ecc000ba6638d9
thelucky_owl <1014147@hr.nl>
2023-01-17 22:54:57 +0100

    added fun animation

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,17 @@
-import {  Container } from "react-bootstrap"
-// import sunImage from "../assets/images/sun.png"
+
+import { motion,useScroll,useTransform,useSpring } from "framer-motion"
 export const Banner =()=>{
+    const {scrollY} = useScroll();
+    const y2 = useTransform(scrollY, [0, 300], [0, 550]);
+    const y2Spring = useSpring(y2, {
+        stiffness: 100,
+        damping: 30,
+        restDelta: 0.001
+      });
     return(
         <section className="banner" id="banner">
-            <div className="sun">
-                {/* <img src={sunImage} alt="sun" className="sun"></img> */}
-            </div>
+            <motion.div style={{y:y2Spring}}   className="sun">
+            </motion.div>
         </section>
     )
 }
