9ff7220c3990fede13d143e18d4c5b2e5acada19
thelucky_owl <1014147@hr.nl>
2023-01-17 22:53:55 +0100

    updated project information

--- src/assets/projectsInfo.js
+++ src/assets/projectsInfo.js
@@ -5,34 +5,30 @@ import img3 from '../assets/images/project-img3.png'
 
 export const projectsInfo =[
     {   id:"1",
-        title:"project 1",
-        description:"the first project",
+        title:"Appcessibility",
+        description:"",
         imgUrl:img1,
-        detailDescription: "this is a much longer description about the project1",
-        showing: false
+        detailDescription: "Appcessibility is a mobile application, made by me and a team of classmates. The app was made for the organisation accessibility, who preform audit on websites and locations to see how accessible they are. The app is supposed to support volunteers with a vision impairment in filling out a questionnaire about locations theyve visited.",
+        showing: false,
+        carouselImgUrls:[img1,img2,img3]
     },
     {
         id:"2",
-        title:"project 2",
-        description:"the second project",
+        title:"Routeco",
+        description:"",
         imgUrl:img2,
-        detailDescription: "this is a much longer description about the project2",
-        showing: false
+        detailDescription: "Routeco is a mobile application, made by me and a team of classmates.  The app was made to collect data about the biodiversity of Rotterdam. User of the app can photograph plants, the app will then figure out what plant this is with image recognition. With the collection of this data a rough idea of the biodiversity in Rotterdam is created.",
+        showing: false,
+        carouselImgUrls:[img1,img2,img3]
     },
     {
         id:"3",
-        title:"project 3",
-        description:"the third project",
+        title:"Automatic trashcan",
+        description:"",
         imgUrl:img3,
-        detailDescription: "this is a much longer description about the project3",
-        showing: false
+        detailDescription: "This Automatic trashcan is the first project i worked on in my study at Rotterdam university of applied sciences. This trashcan was made for a dentist office where the trashcan they had, that was under their desk, couldn’t open completely. It was made using a Circuit Playground Express, a Crickit and of course a trashcan.",
+        showing: false,
+        carouselImgUrls:[img1,img2,img3]
     },
-    {
-        id:"4",
-        title:"project 4",
-        description:"the fourth project",
-        imgUrl:img3,
-        detailDescription: "this is a much longer description about the project4",
-        showing: false
-    },
+    
 ];
