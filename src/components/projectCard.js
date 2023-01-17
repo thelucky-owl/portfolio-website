7a245848b6e45fb151e022ec8a9797b8b14436cc
thelucky_owl <1014147@hr.nl>
2023-01-17 22:57:37 +0100

    added classname to image

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -4,7 +4,7 @@ export const ProjectCard = ({id,title, description, imgUrl})=>{
     return(
         
             <div className="img-box">
-                <img src={imgUrl} alt="project" />
+                <img className="project-card-img" src={imgUrl} alt="project" />
                 <div className="proj-text">
                     <h4>{title}</h4>
                     <span>{description}</span>
