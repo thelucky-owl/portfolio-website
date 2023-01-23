d227ca8116a20bbe30e1085faa030ab1d2dcd45a
thelucky_owl <1014147@hr.nl>
2023-01-23 23:25:51 +0100

    grammar correction

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -5,7 +5,7 @@ return(
     <section className="about-me" id="about-me">
         <Container>
             <h2 id="about-me-title">About me</h2>
-            <p>Hello! I'm Fela, a third year student CMGT at Rotterdam university of applied sciences. In my study ive done ux design, front end and back-end development, mainly javascript and php. In my free time I love to go on walks with my dog, as well as play tabletop rpgs with my friends or play video games.</p>
+            <p>Hello! I'm Fela, a third year student CMGT at Rotterdam university of applied sciences. In my study i've done ux design, front end and back-end development, mainly javascript and php. In my free time I love to go on walks with my dog, as well as play tabletop rpgs with my friends or play video games.</p>
         </Container>
     </section>
 )
