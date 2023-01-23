19f94275290929c88e9de48067a1f70eff95434c
thelucky_owl <1014147@hr.nl>
2023-01-23 23:26:11 +0100

    commented out cv and home/logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,14 +7,14 @@ export const NavBar = () =>{
    return(
     <Navbar bg="dark" variant="dark">
     <Container>
-      <Navbar.Brand href="#home">
+      {/* <Navbar.Brand href="#home"> */}
         Logo
         {/* <img src={} alt="Logo"></img> */}
-      </Navbar.Brand>
+      {/* </Navbar.Brand> */}
       <Nav className="me-auto">
         <Nav.Link href="#about-me">About me</Nav.Link>
         <Nav.Link href="#projects">Werk</Nav.Link>
-        <Nav.Link href="#CV">CV</Nav.Link>
+        {/* <Nav.Link href="#CV">CV</Nav.Link> */}
       </Nav>
     </Container>
   </Navbar>
